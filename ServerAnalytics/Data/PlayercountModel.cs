796674813000010549b4ece0a3719ef1a12ddc7b
Feriixu <39712181+Feriixu@users.noreply.github.com>
2021-01-30 02:59:55 +0100

    Initial commit

---
+++ ServerAnalytics/Data/PlayercountModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerAnalytics.Data
+{
+    public class PlayercountModel
+    {
+        public int id { get; set; }
+        public DateTimeOffset timestamp { get; set; }
+        public int count { get; set; }
+    }
+}
